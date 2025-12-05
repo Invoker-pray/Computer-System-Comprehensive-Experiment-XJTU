1ba96fd645da4b9fa28cbb5f79d49cd2adc6d06f
Invoker-pray <jiaohongbao04@gmail.com>
2025-12-05 23:42:10 +0800

    Fri Dec  5 11:42:10 PM CST 2025

---
+++ exp6/env_prepare.sh
@@ -0,0 +1,8 @@
+git clone https://gitee.com/wlx419/gem5-salam.git
+
+sudo cp -r ../exp1/gem5 .
+
+docker build . --file Dockerfile --build-arg BUILD_TYPE="opt"
+
+#docker pull ubuntu:20.04
+#docker run -it -v "$(pwd)"/:/workstationhere ubuntu:20.04 bash -c "bash /workstationhere/run20.04.sh"
--- exp6/run_lab6.sh
+++
@@ -1,6 +0,0 @@
-git clone https://gitee.com/wlx419/gem5-salam.git
-
-sudo cp -r ../exp1/gem5 .
-
-docker pull ubuntu:20.04
-docker run -it -v "$(pwd)"/:/workstationhere ubuntu:20.04 bash -c "bash /workstationhere/run20.04.sh"
