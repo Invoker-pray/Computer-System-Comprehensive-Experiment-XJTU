63f871abd3838de86fedcb689194e205ff216977
Invoker-pray <jiaohongbao04@gmail.com>
2025-12-14 19:39:45 +0800

    Sun Dec 14 07:39:45 PM CST 2025

--- exp6/run.sh
+++ exp6/run.sh
@@ -1,9 +1,25 @@
+cd /gem5-SALAM
+
 rm -rf /gem5-SALAM/benchmarks/sys_validation/qkv
 cp /workstationhere/run_sim.sh /gem5-SALAM
 
-cp -r /workstationhere/qkv_dimention_64_head_4 /gem5-SALAM/benchmarks/sys_validation/qkv
+cp -r /workstationhere/transformer_dimention_64_head_4 /gem5-SALAM/benchmarks/sys_validation/qkv
 
 cd /gem5-SALAM
+
+echo "transformer dimention 64 head 4 DerivO3CPU"
 bash run.sh
 
-mv BM_ARM_OUT /workstationhere/result/DerivO3CPU
+mv BM_ARM_OUT /workstationhere/result/DerivO3CPU_transformer_dimention_64_head_4
+
+echo "transformer dimention 64 head 4 MinorCPU"
+sed -i "s/cpu-type=DerivO3CPU/cpu-type=MinorCPU/" tools/run_system.sh
+bash run.sh
+
+mv BM_ARM_OUT /workstationhere/result/MinorCPU_transformer_dimention_64_head_4
+
+echo "transformer dimention 64 head 4 TimingSimpleCPU"
+sed -i "s/cpu-type=MinorCPU/cpu-type=TimingSimpleCPU/" tools/run_system.sh
+bash run.sh
+
+mv BM_ARM_OUT /workstationhere/result/TimingSimpleCPU_transformer_dimention_64_head_4
